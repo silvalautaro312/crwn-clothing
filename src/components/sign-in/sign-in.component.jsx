b0b50f14845977c5ba5e727300c56d70d622fbdd
Lauti312 <lauti.silva.ls@gmail.com>
2021-05-27 20:51:13 -0300

    camboio el tipo de boton Sign in a button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -60,7 +60,7 @@ render () {
                 required 
                 />
                 <div className='buttons'>
-                <CustomButton type='submit'> Sign in </CustomButton> 
+                <CustomButton type='button'> Sign in </CustomButton> 
                 <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
                 Sign in with Google
                 </CustomButton> 
