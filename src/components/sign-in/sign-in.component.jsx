00f7f608e66c685f2bb66d688acf7657c137d83e
Lauti312 <lauti.silva.ls@gmail.com>
2021-06-24 16:19:01 -0300

    cambio todo a styled-components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,11 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss';
+import {
+    SignInContainer,
+    SignInTitle,
+    ButtonsBarContainer
+} from './sign-in.styles';
 
 class SignIn extends React.Component {
     constructor(props) { 
@@ -38,9 +42,9 @@ handleChange = event => {
 
 render () {
     return ( 
-        <div className='sign-in'>
-            <h2>I already have an account</h2>
-            <span>Sing in with your email and password</span>
+        <SignInContainer>
+            <SignInTitle>I already have an account</SignInTitle>
+            <span>Sign in with your email and password</span>
 
             <form onSubmit={this.handleSubmit}>
                 <FormInput 
@@ -59,14 +63,14 @@ render () {
                     label='password'
                     required 
                 />
-                <div className='buttons'>
+                <ButtonsBarContainer>
                     <CustomButton type='submit'> Sign in </CustomButton> 
                     <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
                         Sign in with Google
                     </CustomButton> 
-                </div>
+                </ButtonsBarContainer>
             </form>
-        </div>
+        </SignInContainer>
         );
     }
 }
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -5,7 +5,7 @@ import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import './sign-up.styles.scss';
+import { SignUpContainer, SignUpTitle } from './sign-up.styles';
 
 class SignUp extends React.Component {
     constructor(){
@@ -54,10 +54,10 @@ class SignUp extends React.Component {
     render() {
         const { displayName, email, password, confirmPassword } = this.state;
         return(
-            <div className='sign-up'>
-                <h2 className='title'>
+            <SignUpContainer>
+                <SignUpTitle>
                 I do not have a account
-                </h2>
+                </SignUpTitle>
                 <span>Sign up with your email and password</span>
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
                     <FormInput
@@ -95,7 +95,7 @@ class SignUp extends React.Component {
                     />
                     <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
-            </div>
+            </SignUpContainer>
         );
     }
 }
