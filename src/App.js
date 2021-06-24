6e692a4789448f23575dc1dc664cc6330b77beee
Lauti312 <lauti.silva.ls@gmail.com>
2021-06-24 18:11:58 -0300

    agrego collecion de datos del shop al firestore

--- src/App.js
+++ src/App.js
@@ -12,9 +12,14 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component'; 
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { 
+  auth, 
+  createUserProfileDocument 
+} from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
+
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
