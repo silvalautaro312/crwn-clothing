b310c915847b885079d04e400b0896f97b64747a
Lauti312 <lauti.silva.ls@gmail.com>
2021-06-29 16:40:58 -0300

    empiezo con redux-sagas y arreglo tabulacion en codigo

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk];
+const SagaMiddleware = createSagaMiddleware();
+
+const middlewares = [SagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+SagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
